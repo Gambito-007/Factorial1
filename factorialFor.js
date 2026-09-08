d0a6a18ef936d3b133ad36f51d2145d6ccd63f78
Gambito-007 <carloszan584@gmail.com>
2026-09-08 12:39:17 -0600

    Update factorialFor.js

--- factorialFor.js
+++ factorialFor.js
@@ -8,6 +8,16 @@ function factorial(n) {
 
 // Ejemplo de uso:
 console.log(factorial(5)); // Imprime 120
-console.log(factorial(33)); // Imprime 120
-console.log(factorial(35)); // Imprime 120
+
+function factorial2(n) {
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+// Ejemplo de uso:
+console.log(factorial2(5)); // Imprime 120
+
 
